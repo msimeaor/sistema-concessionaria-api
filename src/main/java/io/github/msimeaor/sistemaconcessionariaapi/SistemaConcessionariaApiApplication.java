dec1c333c22a7b736145bbbb1fa55deb884d9f7a
Matheus <maatsimeao@gmail.com>
2023-05-04 14:11:50 -0300

    Initial commit

---
+++ src/main/java/io/github/msimeaor/sistemaconcessionariaapi/SistemaConcessionariaApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.msimeaor.sistemaconcessionariaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaConcessionariaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaConcessionariaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/msimeaor/sistemaconcessionariaapi/SistemaConcessionariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.msimeaor.sistemaconcessionariaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaConcessionariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
