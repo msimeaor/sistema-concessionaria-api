28698b936441b970d45b76e3104dc6a146346338
Matheus <maatsimeao@gmail.com>
2023-06-12 14:53:24 -0300

    projeto finalizado

--- src/main/java/io/github/msimeaor/sistemaconcessionariaapi/domain/dto/ItemPedidoDTO.java
+++ src/main/java/io/github/msimeaor/sistemaconcessionariaapi/domain/dto/ItemPedidoDTO.java
@@ -3,8 +3,9 @@ package io.github.msimeaor.sistemaconcessionariaapi.domain.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Positive;
 import java.util.UUID;
 
 @Getter
@@ -15,7 +16,8 @@ public class ItemPedidoDTO {
   private UUID produto;
 
   @NotNull(message = "{model.message.null}")
-  @PositiveOrZero(message = "{model.message.negative.value}")
+  @Positive(message = "{model.message.negative.value}")
+  @Min(1)
   private Integer quantidade;
 
 }
